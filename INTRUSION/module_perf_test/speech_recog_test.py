c1ef0ce4f9da7b5ec3b7bfbf75a0ba09225b8e7d
darrengjc <darrengjc@gmail.com>
2023-08-20 16:29:22 +0800

    minor commit

--- INTRUSION/module_perf_test/speech_recog_test.py
+++ INTRUSION/module_perf_test/speech_recog_test.py
@@ -58,15 +58,4 @@ def recognizerTest():
     df_results = pd.DataFrame(results)
     print(df_results)
 
-    mean_abs_ggl = df_results["Google WER%"].abs().mean()
-    mean_abs_vosk = df_results["Vosk WER%"].abs().mean()
-
-    
-    # Calculate the total average percentage for each column
-    percent_ggl = mean_abs_ggl 
-    percent_vosk = mean_abs_vosk
-
-    print(f"Total Average WER Percentage for Google: {percent_ggl:.2f}%")
-    print(f"Total Average WER Percentage for Vosk: {percent_vosk:.2f}%")
-
 recognizerTest()
